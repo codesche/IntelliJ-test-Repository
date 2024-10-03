d50669f1532f3d8deee096603004ba22f8a857fa
codesche <codesche@gmail.com>
2024-10-04 06:27:53 +0900

    Project Setting

---
+++ src/main/java/com/intellijtestproject/IntellijTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.intellijtestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntellijTestProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/intellijtestproject/IntellijTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellijtestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellijTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
